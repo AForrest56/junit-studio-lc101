c520ed78f8717d8241bb3f21dda0fc4ed1f266a7
Ashley <aforrest56@gmail.com>
2022-09-12 20:32:39 -0500

    Studio Done

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,68 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+   // @Test
+   // public void emptyTest() {
+       // assertEquals(true, true);
+   // }
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void bracketsAroundWord(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[word]"));
     }
 
+    @Test
+    public void bracketsWithinWord(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("w[ord]"));
+    }
+
+    @Test
+    public void bracketsBeforeWord(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]word"));
+    }
+
+    @Test
+    public void noBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void emptyBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void nestedBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[w[or]d]"));
+    }
+
+    @Test
+    public void multipleWordsWithinBracket(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("a str[ing of wor]ds"));
+    }
+    @Test
+    public void missingClosingBracket(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[word"));
+    }
+
+    @Test
+    public void missingOpeningBracket(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("word]"));
+    }
+
+    @Test
+    public void closingBracketBeforeOpeningBracket(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]word["));
+    }
+
+    @Test
+    public void mismatchBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]][[[][][]"));
+    }
+
+    @Test
+    public void moreClosingBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[word]]"));
+    }
 
 }
